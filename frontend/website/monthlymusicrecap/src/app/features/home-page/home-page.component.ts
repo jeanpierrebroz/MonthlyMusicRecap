ce8b6106dff1a6843bd9c7e270e7369526bf31b1
jeanpierrebroz <jeanpierrebroz@gmail.com>
2025-08-22 14:30:03 -0600

    added old jupyter notebook

--- frontend/website/monthlymusicrecap/src/app/features/home-page/home-page.component.ts
+++ frontend/website/monthlymusicrecap/src/app/features/home-page/home-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { GraphViewerComponent } from '../graph-viewer/graph-viewer.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [GraphViewerComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
