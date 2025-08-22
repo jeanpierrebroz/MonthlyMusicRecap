8e8d9dd148105cfcb79a6d80dbf8ee01d9459b5b
jeanpierrebroz <jeanpierrebroz@gmail.com>
2025-08-22 12:17:00 -0600

    added vis service

--- frontend/website/monthlymusicrecap/src/app/services/visualization/visualization.service.ts
+++ frontend/website/monthlymusicrecap/src/app/services/visualization/visualization.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class VisualizationServiceService {
+export class VisualizationService {
 
   constructor() { }
 }
