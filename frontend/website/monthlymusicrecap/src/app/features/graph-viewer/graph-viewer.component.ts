73a3bb0e609b6cf93cc66c9fcbf219f44e8e48bf
jeanpierrebroz <jeanpierrebroz@gmail.com>
2025-08-22 15:32:47 -0600

    the voices lowkey ts

--- frontend/website/monthlymusicrecap/src/app/features/graph-viewer/graph-viewer.component.ts
+++ frontend/website/monthlymusicrecap/src/app/features/graph-viewer/graph-viewer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ApexCharts } from 'apexcharts'
 
 @Component({
   selector: 'app-graph-viewer',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class GraphViewerComponent {
 
+    @Input() graphData: any;
+
+    ngAfterViewInit() {
+        var chart = new ApexCharts()
+    }
 }
