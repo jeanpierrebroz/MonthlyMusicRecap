9f6c80582492b41bf5727ca4946c610b3a9c9cc2
jeanpierrebroz <jeanpierrebroz@gmail.com>
2025-08-22 16:40:24 -0600

    added lists, need to implement

--- frontend/website/monthlymusicrecap/src/app/features/home-page/home-page.component.ts
+++ frontend/website/monthlymusicrecap/src/app/features/home-page/home-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { GraphViewerComponent } from '../graph-viewer/graph-viewer.component';
+import {MatListModule} from '@angular/material/list';
+
 
 @Component({
   selector: 'app-home-page',
-  imports: [GraphViewerComponent],
+  imports: [GraphViewerComponent, MatListModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
