f7b05ee3782ae6eccff42e52f54dc9b742848589
jeanpierrebroz <jeanpierrebroz@gmail.com>
2025-08-22 16:05:03 -0600

    that was lowkey awful

--- frontend/website/monthlymusicrecap/src/app/features/graph-viewer/graph-viewer.component.ts
+++ frontend/website/monthlymusicrecap/src/app/features/graph-viewer/graph-viewer.component.ts
@@ -1,17 +1,68 @@
-import { Component, Input } from '@angular/core';
-import { ApexCharts } from 'apexcharts'
+import { Component, Input, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { EChartsOption } from 'echarts';
+import { NgxEchartsDirective, provideEchartsCore } from 'ngx-echarts';
+import * as echarts from 'echarts/core';
+import { LineChart } from 'echarts/charts';
+import { GridComponent } from 'echarts/components';
+import { CanvasRenderer } from 'echarts/renderers';
+echarts.use([LineChart, GridComponent, CanvasRenderer]);
 
 @Component({
   selector: 'app-graph-viewer',
-  imports: [],
+  imports: [NgxEchartsDirective],
   templateUrl: './graph-viewer.component.html',
-  styleUrl: './graph-viewer.component.scss'
+  styleUrl: './graph-viewer.component.scss',
+  providers: [
+      provideEchartsCore({ echarts }),
+  ]
 })
-export class GraphViewerComponent {
+export class GraphViewerComponent implements OnInit {
 
-    @Input() graphData: any;
+    @Input() chart: any;
+    chartOption: EChartsOption = {};
+    isBrowser: boolean;
 
-    ngAfterViewInit() {
-        var chart = new ApexCharts()
+    constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+        this.isBrowser = isPlatformBrowser(this.platformId);
+    }
+
+    ngOnInit() {
+        if (!this.isBrowser) {
+            return;
+        }
+
+        // Example line chart configuration
+        this.chartOption = {
+            title: {
+                text: 'Monthly Music Stats'
+            },
+            tooltip: {
+                trigger: 'axis'
+            },
+            legend: {
+                data: ['Songs Played']
+            },
+            xAxis: {
+                type: 'category',
+                data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [{
+                name: 'Songs Played',
+                type: 'line',
+                data: [120, 132, 101, 134, 90, 230],
+                smooth: true
+            }]
+        };
+
+        // If you have data from input, use it instead
+        if (this.chart) {
+            this.chartOption = this.chart;
+        }
+
+
     }
 }
