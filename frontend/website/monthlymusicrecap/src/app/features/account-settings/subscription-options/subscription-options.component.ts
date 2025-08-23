79f2b64b399fcfb5d82689b63e20ca31e6d22f9d
jeanpierrebroz <jeanpierrebroz@gmail.com>
2025-08-22 23:08:24 -0600

    added subscription options component

---
+++ frontend/website/monthlymusicrecap/src/app/features/account-settings/subscription-options/subscription-options.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscription-options',
+  imports: [],
+  templateUrl: './subscription-options.component.html',
+  styleUrl: './subscription-options.component.scss'
+})
+export class SubscriptionOptionsComponent {
+
+}
