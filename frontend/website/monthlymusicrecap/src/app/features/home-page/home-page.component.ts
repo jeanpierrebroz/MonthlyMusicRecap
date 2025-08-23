c975140f88073af81dc88d43984b16fb88604f34
jeanpierrebroz <jeanpierrebroz@gmail.com>
2025-08-23 00:29:37 -0600

    added get started button

--- frontend/website/monthlymusicrecap/src/app/features/home-page/home-page.component.ts
+++ frontend/website/monthlymusicrecap/src/app/features/home-page/home-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { GraphViewerComponent } from '../graph-viewer/graph-viewer.component';
 import { SubscriptionOptionsComponent } from "../account-settings/subscription-options/subscription-options.component";
-
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home-page',
-  imports: [GraphViewerComponent, SubscriptionOptionsComponent],
+  imports: [GraphViewerComponent, SubscriptionOptionsComponent, MatButtonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
