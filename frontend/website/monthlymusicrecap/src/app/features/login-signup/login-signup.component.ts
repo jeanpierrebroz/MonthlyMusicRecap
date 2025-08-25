907331fb4ea730e837075f77e22943f431fe9464
jeanpierrebroz <jeanpierrebroz@gmail.com>
2025-08-24 20:40:20 -0600

    remove nested login, create main login/signup component

---
+++ frontend/website/monthlymusicrecap/src/app/features/login-signup/login-signup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-signup',
+  imports: [],
+  templateUrl: './login-signup.component.html',
+  styleUrl: './login-signup.component.scss'
+})
+export class LoginSignupComponent {
+
+}
